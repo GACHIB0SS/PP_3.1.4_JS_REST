63f376f8ebde812a1333d4d8a65d974a1ff01487
GACHIB0SS <der.confederate@gmail.com>
2022-09-16 00:54:30 +0500

    -lombok Role

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -1,17 +1,12 @@
 package ru.kata.spring.boot_security.demo.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
 
 
-
 @Entity
 @Table(name = "roles")
-@Data
-@NoArgsConstructor
 public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,8 +18,32 @@ public class Role implements GrantedAuthority {
         this.id = id;
     }
 
+    public Role() {
+    }
+
     @Override
     public String getAuthority() {
         return getName();
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String toString() {
+        return "Role(id=" + this.getId() + ", name=" + this.getName() + ")";
     }
 }
